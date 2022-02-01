31dc6c258692cb6de9b8470766bc51b4fde9191e
Chunkybanana <chunkybananagithub@gmail.com>
2022-02-02 11:59:19 +1300

    fixed brainfuck interpreter

--- langs/bf.js
+++ langs/bf.js
@@ -1,43 +1,25 @@
 DSO.defineMode('brainfuck',async (code,input,args,output,debug) => {
     var tape = [0],tapeIndex = 0,tapeLimit = Infinity,cellLimit = 256,inputIndex = 0,brackets = [];
+    input = [...input]
     if(args.includes('6')) cellLimit = 65536;
     if(args.includes('t')) tapeLimit = 256;
     if(args.includes('T')) tapeLimit = 65536;
-    for(var codeIndex = 0; codeIndex < code.length; codeIndex++) {
-        var char = code[codeIndex]
-        if(char == '+') tape[tapeIndex] = (tape[tapeIndex] + 1) % cellLimit
-        if(char == '-') tape[tapeIndex] = (tape[tapeIndex] + cellLimit - 1) % cellLimit;
-        if(char == '.') output(String.fromCharCode(tape[tapeIndex]));
-        if(char == ',') tape[tapeIndex] = input[inputIndex++]
-        if(char == '>'){
-            tapeIndex++;
-            if(tapeIndex > tape.length){
-                if(tape.length > tapeLimit){
-                    tapeIndex = 0
-                } else {
-                    tape.push(0)
-                }                
-            }
-        }
-        if(char == '<'){
-            tapeIndex--;
-            if(tapeIndex == -1){
-                debug('Warning: Pointer fallen of left end of tape, yeeted onto right end\n')
-                if(tape.length > tapeLimit){
-                    tapeIndex = tape.length - 1
-                } else {
-                    tape.unshift(0)
-                    tapeIndex--;
-                }
-            }
-        }
-        if(char == '[') brackets.push(codeIndex)
-        if(char == ']'){
-            if(tape[tapeIndex]){
-                codeIndex = brackets[brackets.length-1]
-            } else {
-                brackets.pop()
-            }
+    let codeTable = {
+        '+': 'tape[tapeIndex] = (tape[tapeIndex] + 1) % cellLimit',
+        '-': 'tape[tapeIndex] = tape[tapeIndex] ? tape[tapeIndex] - 1 : cellLimit - 1',
+        '>': 'tapeIndex = (tapeIndex + 1) % tapeLimit',
+        '<': 'tapeIndex = tapeIndex ? tapeIndex - 1 : Number.isFinite(tapeLimit) ? tapeLimit - 1 : tape.unshift(0) && 0',
+        '[': 'while(tape[tapeIndex]){',
+        ']': '}',
+        '.': 'output(String.fromCharCode(tape[tapeIndex]))',
+        ',': 'tape[tapeIndex] = input.shift().charCodeAt()',
+    }
+    let transpiled = '';
+    for(char of code){
+        if(char in codeTable){
+            transpiled += codeTable[char] + '\n'
         }
     }
+    if(args.includes('c')) debug(transpiled);
+    eval(transpiled);
 })
