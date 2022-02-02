74576581ba34ae35f7f0445dcd3f0bbeb694b920
Chunkybanana <chunkybananagithub@gmail.com>
2022-02-03 11:16:04 +1300

    Besterest BQN errors

--- langs/bqn.js
+++ langs/bqn.js
@@ -1,14 +1,10 @@
 fetch('https://raw.githubusercontent.com/mlochbaum/BQN/master/docs/bqn.js').then(x => x.text()).then(text => {
-    eval(text.replace('let bqn', 'window.bqn').replace('let fmt = ','window.fmt = '));
+    eval(text.replace('let bqn', 'window.bqn').replace('let fmt = ','window.fmt = ').replace('let fmtErr = ','window.fmtErr = '));
 }).then(_ => console.log('bqn.js loaded', bqn))
 DSO.defineMode('bqn', async (code,input,args,output,debug) => {
     try {
         output(fmt(bqn(code)))
     } catch(ex) { 
-        try {
-            debug(`${ex.kind} Error: ${ex.message[1]?.join?.('') ?? ex.message.join('')}`)
-        } catch {
-            debug('This program throws an error whose message is misdecoded. Add an issue on the github repo or ping me.')
-        }
+        debug(fmtErr(ex))
     }
 })
