c35f5d2061a2a6cf36cbc1678994cb4c9dadb742
Chunkybanana <chunkybananagithub@gmail.com>
2022-02-06 10:12:47 +1300

    Maybe japt?

--- langs/japt.js
+++ langs/japt.js
@@ -1,10 +1,15 @@
 fetch('https://raw.githubusercontent.com/ETHproductions/japt/master/dependencies/shoco.js').then(x => x.text()).then(text => {
     eval(text);
+    console.log('shoco.js loaded', shoco)
 }).then(() => {
     fetch('https://raw.githubusercontent.com/ETHproductions/japt/master/src/japt-interpreter.js').then(x => x.text()).then(text => {
-        eval(text).replace('var Japt = ','window.Japt = ');
-    });
-}).then(() => console.log('japt.js loaded', Japt));
+        eval(
+            text.replace('var Japt = ','window.Japt = ')
+            .replace(/shoco\./g, 'window.shoco.')
+        );
+        console.log(Japt)
+    }).then(() => console.log('japt.js loaded', Japt)).catch(console.error);
+})
 
 DSO.defineMode('japt', async (code,input,args,output,debug) => {
     Japt.run(code, input, false, null, output, debug);
