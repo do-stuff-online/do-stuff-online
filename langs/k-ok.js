1ad392c17c25f112cf265237ea0a75842ae42334
Chunkybanana <chunkybananagithub@gmail.com>
2022-02-07 17:01:23 +0900

    Revert python stuff, partially add oK

---
+++ langs/k-ok.js
@@ -0,0 +1,13 @@
+fetch('https://johnearnest.github.io/ok/oK.js').then(x => x.text()).then(text => {
+    eval(text.replace('this', 'globalThis'))
+}).then(_ => console.log('oK.js loaded', parse, run, baseEnv, format))
+DSO.defineMode('k-ok',async (code,input,args,output,debug) => {
+    try {
+        if(input) debug('Input not supported')
+        var env = baseEnv();
+    	var ret = run(parse(code), baseEnv());
+        output(format(ret))
+    } catch (ex) {
+        debug(ex)
+    }
+})
