7ca7226a44864806c86280e02aad654ef47b1877
Chunkybanana <chunkybananagithub@gmail.com>
2022-02-07 17:20:56 +0900

    python utils appear to be working (fingers crossed they don't blow up in my face)

--- langs/python-pyodide.js
+++ langs/python-pyodide.js
@@ -1,24 +1,7 @@
-import 'https://cdn.jsdelivr.net/pyodide/v0.19.0/full/pyodide.js'
-let pythonInput = []
-if(!window.pyodide){
-    DSO.startLoad();
-    let time = Date.now()
-    let pyodide = await loadPyodide({
-        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/",
-        stdin: _ => pythonInput.shift() ?? eval('throw "out of input"'),
-        stdout: str => $('output').value += str,
-        stderr: str => $('debug').value += str
-    })
-    console.log('python (pyodide) loaded in %d seconds', (Date.now() - time) / 1000)
-    window.pyodide = pyodide
-    DSO.endLoad();
-}
+loadPyodide()
 DSO.defineMode('python-pyodide', async (code, input, args, output, debug) => {
     try {
-        function py_repr(str){
-            return `"${str.replace(/\\/g,'\\\\').replace(/\n/g,'\\n').replace(/"/g,'\\"')}"`
-        }
-        pythonInput = input.split`\n`
+        window.pythonInput = input.split`\n`
         pyodide.runPython(`exec(${py_repr(code)})`);
     } catch(e){
         debug(e)
---
+++ python-utils.js
@@ -0,0 +1,20 @@
+async function loadPyodide(){
+    await import('https://cdn.jsdelivr.net/pyodide/v0.19.0/full/pyodide.js')
+    window.pythonInput = []
+    if(!window.pyodide){
+        DSO.startLoad();
+        let time = Date.now()
+        let pyodide = await loadPyodide({
+            indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/",
+            stdin: _ => pythonInput.shift() ?? eval('throw "out of input"'),
+            stdout: str => $('output').value += str,
+            stderr: str => $('debug').value += str
+        })
+        console.log('python (pyodide) loaded in %d seconds', (Date.now() - time) / 1000)
+        window.pyodide = pyodide
+        DSO.endLoad();
+    }
+}
+function py_repr(str){
+    return `"${str.replace(/\\/g,'\\\\').replace(/\n/g,'\\n').replace(/"/g,'\\"')}"`
+}
