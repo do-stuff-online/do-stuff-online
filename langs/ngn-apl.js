9b16202a294895b5396f049a2a94746abb859aec
Chunkybanana <chunkybananagithub@gmail.com>
2022-02-08 17:38:39 +0900

    ngn/apl output is amess

--- langs/ngn-apl.js
+++ langs/ngn-apl.js
@@ -6,8 +6,7 @@ DSO.defineMode('ngn-apl', async (code,input,args,output,debug) => {
     input = input.split`\n`
     try {
         output(apl.fmt(apl(code, {
-            in: input.shift,
-            out: output
+            out: v => v && output(v)
         })).join('\n')+'\n')
     } catch(ex) { 
         debug(ex)
