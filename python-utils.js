abf3e6747d1d64f3763d30b446dfe8c7de033bc2
Chunkybanana <chunkybananagithub@gmail.com>
2022-03-19 17:03:29 +0900

    Fixed python
    It was *that simple*?

--- python-utils.js
+++ python-utils.js
@@ -7,8 +7,8 @@ async function loadPyodide(){
         let pyodide = await loadPyodide({
             indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/",
             stdin: _ => pythonInput.shift() ?? eval('throw "out of input"'),
-            stdout: str => $('output').value += str,
-            stderr: str => $('debug').value += str
+            stdout: str => $('output').value += str + '\n',
+            stderr: str => $('debug').value += str + '\n'
         })
         console.log('python (pyodide) loaded in %d seconds', (Date.now() - time) / 1000)
         window.pyodide = pyodide
