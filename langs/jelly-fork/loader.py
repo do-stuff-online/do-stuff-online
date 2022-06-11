144be91fb446ca8fa94592f8f741f9f5b61cb20a
Chunkybanana <chunkybananagithub@gmail.com>
2022-06-11 12:56:52 +1200

    Jelly (fork)
    Only tweak required was messing with sympy

---
+++ langs/jelly-fork/loader.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('./')
+from js import fetch
+
+import os
+os.mkdir("jelly")
+
+async def main():
+    files = await fetch("/langs/jelly/all.txt")
+    files = await files.text()
+    for file in files.split('\n')[:-1]:
+        text = await fetch('https://raw.githubusercontent.com/cairdcoinheringaahing/jellylanguage/master/jelly/' + file)
+        text = await text.text()
+        with open('jelly/' + file, "w") as f:
+            f.write(text)
+    
+    from jelly import code_page, main, try_eval
+
+    global run
+    def run(code, args):
+        args = list(map(try_eval, args or []))
+        result = main(code, args, end='')
+        print(str(result)[:-4])
---
+++ langs/jelly-fork/script.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/cairdcoinheringaahing/jellylanguage.git
+cd jelly/jelly
+ls *.py > ../../all.txt
+cd ../../
+rm -rf jelly
