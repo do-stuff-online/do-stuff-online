8b6beaac85a9508acb062b53f68bf201d10faccc
Chunkybanana <chunkybananagithub@gmail.com>
2022-06-11 16:32:57 +1200

    small patch to python utils

--- python-utils.js
+++ python-utils.js
@@ -1,23 +1,26 @@
 async function loadPyodide(){
     await import('https://cdn.jsdelivr.net/pyodide/v0.19.0/full/pyodide.js')
-    window.pythonInput = []
-    if(!window.pyodide){
-        DSO.startLoad();
-        let time = Date.now()
-        let pyodide = await loadPyodide({
-            indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/",
-            stdin: _ => pythonInput.shift() ?? eval('throw "out of input"'),
-            stdout: str => {
-                if (str != 'Python initialization complete') {
-                    console.log(str);
-                    $('output').value += str + '\n';
-                }
-            },
-            stderr: str => $('debug').value += str + '\n'
-        })
-        console.log('python (pyodide) loaded in %d seconds', (Date.now() - time) / 1000)
-        window.pyodide = pyodide
-        DSO.endLoad();
+    window.pythonInput = [];
+    try {
+        if (!window.pyodide) {
+            DSO.startLoad();
+            let time = Date.now()
+            let pyodide = await loadPyodide({
+                indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/",
+                stdin: _ => pythonInput.shift() ?? eval('throw "out of input"'),
+                stdout: str => {
+                    if (str != 'Python initialization complete') {
+                        $('output').value += str + '\n';
+                    }
+                },
+                stderr: str => $('debug').value += str + '\n'
+            })
+            console.log('python (pyodide) loaded in %d seconds', (Date.now() - time) / 1000)
+            window.pyodide = pyodide
+            DSO.endLoad();
+        }
+    } catch (e) {
+        console.log(e)
     }
 }
 function py_repr(str){
