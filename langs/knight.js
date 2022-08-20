c2a2b6bc8db547755ebb9d45dc6a3393a308075d
Steffan <40404519+Steffan153@users.noreply.github.com>
2022-08-19 20:55:22 -0600

    Update knight.js

--- langs/knight.js
+++ langs/knight.js
@@ -1,4 +1,4 @@
-fetch(https://gist.githubusercontent.com/Steffan153/860aa37d224aeb8ac37a1b2e7b5c5c5f/raw/knight.js').then(x => x.text()).then(eval);
+fetch('https://gist.githubusercontent.com/Steffan153/860aa37d224aeb8ac37a1b2e7b5c5c5f/raw/knight.js').then(x => x.text()).then(eval);
 
 DSO.defineMode('knight', (code,input,args,output,debug) => {
     try {
