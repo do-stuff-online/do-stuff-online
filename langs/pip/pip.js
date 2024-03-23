74ab7b9290a05642391d7214bd4307c100928122
David Loscutoff <dloscutoff@gmail.com>
2024-03-22 21:01:39 -0500

    Use run() instead of pip() The pip() function was recently changed to raise a Python error when something goes wrong with the Pip program. The run() function catches these errors and is a better fit for DSO.

--- langs/pip/pip.js
+++ langs/pip/pip.js
@@ -7,7 +7,7 @@ pyodide.runPythonAsync(await (await fetch('/langs/pip/loader.py')).text() + '\na
 DSO.defineMode("pip", async (code,input,args,output,debug) => {
     try {
         window.pythonInput = input.split`\n`
-        pyodide.globals.get('pip')(code, args)
+        pyodide.globals.get('runpip')(code, args)
     } catch (e) {
         console.log(e)
         debug(e)
